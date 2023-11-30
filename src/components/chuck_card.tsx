52c440bfaa2f76856d69f7bc541027d3e6bb632f
smithaagarwal <smitha.kotian@gmail.com>
2023-11-30 14:52:37 +0000

    add logic to meet requirements 1-4 of the document

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,22 +1,21 @@
 // 💡 You can import assets like images directly and use them in your components:
-import ChuckImage from '../assets/chuck_norris.jpg';
+import ChuckImage from "../assets/chuck_norris.jpg";
 
 interface ChuckCardProps {
-
+  chuckGreeting: string;
 }
 
-const ChuckCard: React.FC<ChuckCardProps> = ({}) => {
+const ChuckCard: React.FC<ChuckCardProps> = ({ chuckGreeting }) => {
+  return (
+    // 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
+    // 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
+    <>
+      {/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
+      <h2>{chuckGreeting}</h2>
 
-	return (
-		// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
-		// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
-		<>
-			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
-			<h2>{ }</h2>
-
-			<img src={'Chuck image here!'} alt='A handsome man' />
-		</>
-	)
-}
+      <img src={ChuckImage} alt="A handsome man" />
+    </>
+  );
+};
 
 export default ChuckCard;
--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,14 @@
-interface ChuckJokeProps {
+import Joke from "../joke";
 
+interface ChuckJokeProps {
+  jokes: Array<Joke>;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
-
-
+const ChuckJoke: React.FC<ChuckJokeProps> = (props) => {
+  const jokes = props.jokes.map((joke) => {
+    return <li key={joke.id}>{joke.joke}</li>;
+  });
+  return <p>{jokes}</p>;
+};
 
 export default ChuckJoke;
